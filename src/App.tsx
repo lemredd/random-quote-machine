8657e81e5dadd83ea5e9d6ab800dad319b3d7c3b
lemredd <68067933+lemredd@users.noreply.github.com>
2023-07-28 07:03:46 +0000

    chore: give IDs `text` and `author`

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,8 @@ import './style.css';
 export default function App() {
   return (
     <div id="quote-box">
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <h1 id="text">Hello StackBlitz!</h1>
+      <p id="author">Start editing to see some magic happen :)</p>
     </div>
   );
 }
