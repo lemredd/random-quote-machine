c448a0f322a4c731f788414d3130913ec9ecb7cf
lemredd <68067933+lemredd@users.noreply.github.com>
2023-07-28 07:41:32 +0000

    feat: add more items in `quotes`

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,18 @@ const quotes = [
     author: 'Richard Bach',
     text: 'The best way to pay for a lovely moment is to enjoy it.',
   },
+  {
+    author: 'George Orwell',
+    text: 'In a time of deceit telling the truth is a revolutionary act.',
+  },
+  {
+    author: 'Oscar Wilde',
+    text: 'A work of art is the unique result of a unique temperament.',
+  },
+  {
+    author: 'John Barrymore',
+    text: 'A man is not old until regrets take the place of dreams.',
+  },
 ];
 
 const get_random_quote = () =>
