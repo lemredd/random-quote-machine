2a5b4f8e9d06c6380df6043e59fea0e7f936a28f
lemredd <68067933+lemredd@users.noreply.github.com>
2023-07-28 07:02:59 +0000

    chore: give `id` of `quote-box`

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './style.css';
 
 export default function App() {
   return (
-    <div>
+    <div id="quote-box">
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
     </div>
