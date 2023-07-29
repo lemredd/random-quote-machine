7ace403c89e1259701aceaaa6c31ef121e1745b8
Lem Redd <lmoa.jhdp@gmail.com>
2023-07-29 12:23:06 +0800

    style: replace indents with tab

--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,11 @@ import { createRoot } from 'react-dom/client';
 
 import App from './App';
 
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(rootElement);
 
 root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+	<StrictMode>
+		<App />
+	</StrictMode>
 );
