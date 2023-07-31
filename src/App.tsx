0e70903a40c089f9cfcbf1b4a710d3a38f9ca4fe
lemredd <68067933+lemredd@users.noreply.github.com>
2023-07-31 04:02:21 +0000

    refactor(helper): split into `quotes.ts`

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,8 @@
 import * as React from 'react';
+
+import { get_random_quote } from './helpers/quotes';
+
 import './style.css';
-
-const quotes = [
-  {
-    author: 'Pablo Picasso',
-    text: 'Art is the lie that enables us to realize the truth.',
-  },
-  {
-    author: 'Charlie Gilkey',
-    text: 'If it matters to you, you’ll find a way.',
-  },
-  {
-    author: 'Richard Bach',
-    text: 'The best way to pay for a lovely moment is to enjoy it.',
-  },
-  {
-    author: 'George Orwell',
-    text: 'In a time of deceit telling the truth is a revolutionary act.',
-  },
-  {
-    author: 'Oscar Wilde',
-    text: 'A work of art is the unique result of a unique temperament.',
-  },
-  {
-    author: 'John Barrymore',
-    text: 'A man is not old until regrets take the place of dreams.',
-  },
-];
-
-const get_random_quote = () =>
-  quotes[Math.floor(Math.random() * (quotes.length - 1))];
 
 export default function App() {
   const [quote, set_quote] = React.useState(get_random_quote());
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,14 @@
 import * as React from 'react';
 import { createRoot } from 'react-dom/client';
 
+// If repo is cloned locally, this error won't appear.
 import App from './App.tsx';
 
 const rootElement = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(rootElement);
 
 root.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
