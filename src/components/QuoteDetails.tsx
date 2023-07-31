de549fbbd51c2a04b971ccb69cb225c49c5d08e5
Lem Redd <lmoa.jhdp@gmail.com>
2023-07-31 14:53:36 +0800

    feat(component): use `h4` for `#author`

--- src/components/QuoteDetails.tsx
+++ src/components/QuoteDetails.tsx
@@ -2,14 +2,14 @@ import * as React from 'react';
 import type { Quote as Props } from '../types';
 
 export default function QuoteDetails({ author, text }: Props) {
-  return (
-    <div>
-      <h1 id="text">
-        <strong>"{text}"</strong>
-      </h1>
-      <p id="author" className="mt-3">
-        {author}
-      </p>
-    </div>
-  );
+	return (
+		<div>
+			<h1 id="text">
+				<strong>"{text}"</strong>
+			</h1>
+			<h4 id="author" className="mt-3">
+				{author}
+			</h4>
+		</div>
+	);
 }
