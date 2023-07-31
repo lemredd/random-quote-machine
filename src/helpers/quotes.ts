510df06428419d75178b847cbd4a727fb414adc2
Lem Redd <lmoa.jhdp@gmail.com>
2023-07-31 13:09:48 +0800

    style: use tabs to indent

--- src/helpers/quotes.ts
+++ src/helpers/quotes.ts
@@ -1,29 +1,29 @@
 const quotes = [
-  {
-    author: 'Pablo Picasso',
-    text: 'Art is the lie that enables us to realize the truth.',
-  },
-  {
-    author: 'Charlie Gilkey',
-    text: 'If it matters to you, you’ll find a way.',
-  },
-  {
-    author: 'Richard Bach',
-    text: 'The best way to pay for a lovely moment is to enjoy it.',
-  },
-  {
-    author: 'George Orwell',
-    text: 'In a time of deceit telling the truth is a revolutionary act.',
-  },
-  {
-    author: 'Oscar Wilde',
-    text: 'A work of art is the unique result of a unique temperament.',
-  },
-  {
-    author: 'John Barrymore',
-    text: 'A man is not old until regrets take the place of dreams.',
-  },
+	{
+		author: 'Pablo Picasso',
+		text: 'Art is the lie that enables us to realize the truth.',
+	},
+	{
+		author: 'Charlie Gilkey',
+		text: 'If it matters to you, you’ll find a way.',
+	},
+	{
+		author: 'Richard Bach',
+		text: 'The best way to pay for a lovely moment is to enjoy it.',
+	},
+	{
+		author: 'George Orwell',
+		text: 'In a time of deceit telling the truth is a revolutionary act.',
+	},
+	{
+		author: 'Oscar Wilde',
+		text: 'A work of art is the unique result of a unique temperament.',
+	},
+	{
+		author: 'John Barrymore',
+		text: 'A man is not old until regrets take the place of dreams.',
+	},
 ];
 
 export const get_random_quote = () =>
-  quotes[Math.floor(Math.random() * (quotes.length - 1))];
+	quotes[Math.floor(Math.random() * (quotes.length - 1))];
