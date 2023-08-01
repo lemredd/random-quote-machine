1ab864f8bf27d9465bdec854aa5f8e820139d3c3
Lem Redd <lmoa.jhdp@gmail.com>
2023-08-01 17:48:15 +0800

    feat(component): modify opacity within `useEffect`
    When `is_fetching_quote` changes, the `quote_details_container`'s
    opacity is modified.

--- src/components/QuoteDetails.tsx
+++ src/components/QuoteDetails.tsx
@@ -7,6 +7,10 @@ interface Props extends Quote {
 
 export default function QuoteDetails({ author, text, is_fetching_quote }: Props) {
 	const quote_details_container = React.useRef<HTMLDivElement>(null)
+	React.useEffect(() => {
+		if (is_fetching_quote) quote_details_container.current.style.opacity = "0"
+		else quote_details_container.current.style.opacity = "1"
+	}, [is_fetching_quote])
 	return (
 		<div>
 			<h1 id="text">
